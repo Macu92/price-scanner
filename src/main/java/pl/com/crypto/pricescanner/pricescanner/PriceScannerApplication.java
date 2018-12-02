5ed5f53162827721d4ba73a05894c704b4b0fc3b
Maciej Ziniewicz <ziniewiczmaciej@gmail.com>
2018-12-03 00:00:31 +0100

    Init commit

---
+++ src/main/java/pl/com/crypto/pricescanner/pricescanner/PriceScannerApplication.java
@@ -0,0 +1,12 @@
+package pl.com.crypto.pricescanner.pricescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceScannerApplication.class, args);
+	}
+}
